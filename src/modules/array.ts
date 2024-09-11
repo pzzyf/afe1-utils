8a3b8d3bff390e094ef0df3a02eafb0d401a9044
pzzyf <2279948211@qq.com>
2024-09-11 17:13:41 +0800

    feat: workflows

--- src/modules/array.ts
+++ src/modules/array.ts
@@ -1,6 +1,7 @@
 // src/modules/array.ts
 /**
  * The function `arr_unique` removes duplicate elements from an array in TypeScript.
+ * @public
  * @param {T[]} arr - The `arr` parameter is an array of elements of type `T`.
  * @returns The function `arr_unique` returns an array with all duplicate elements removed from the
  * input array `arr`.
